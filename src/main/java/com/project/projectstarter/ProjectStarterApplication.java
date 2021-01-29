0d0a925757b4adb7fb1beffd35a088a209ffc4c8
Ratul Sharker <Sharker.ratul.08@gmail.com>
2021-01-30 01:41:06 +0600

    - empty project added.

---
+++ src/main/java/com/project/projectstarter/ProjectStarterApplication.java
@@ -0,0 +1,13 @@
+package com.project.projectstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/projectstarter/ProjectStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.projectstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
