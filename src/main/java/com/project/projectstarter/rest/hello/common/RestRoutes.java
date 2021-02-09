dde644a985496b724b17f14f201c3bd44029a9db
Ratul Sharker <Sharker.ratul.08@gmail.com>
2021-02-10 00:10:55 +0600

    - constant routes file added.

---
+++ src/main/java/com/project/projectstarter/rest/hello/common/RestRoutes.java
@@ -0,0 +1,13 @@
+package com.project.projectstarter.rest.hello.common;
+
+public final class RestRoutes {
+
+    private RestRoutes() {
+    }
+
+    private static final String ROOT = "api";
+
+    // hello
+    public static final String HELLO = ROOT + "/hello";
+
+}
