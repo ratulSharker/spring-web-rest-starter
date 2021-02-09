bbee305186ae10eaac8c687e55e8363a6170a098
Ratul Sharker <Sharker.ratul.08@gmail.com>
2021-02-10 00:09:10 +0600

    - `HelloController` moved to `WebHelloController`.

--- src/main/java/com/project/projectstarter/web/hello/WebHelloController.java
+++ src/main/java/com/project/projectstarter/web/hello/WebHelloController.java
@@ -1,10 +1,10 @@
-package com.project.projectstarter.hello;
+package com.project.projectstarter.web.hello;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HelloController {
+public class WebHelloController {
 
     @GetMapping(value = "/hello")
     public String helloIndex() {
